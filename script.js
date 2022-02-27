a5979b859a1d95539d0aba0198f1c87b3b470eb0
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-02-28 02:21:58 +0400

    TahaThinks Solves IBM Challenge

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let amount = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let currentYear = new Date().getFullYear();
+    let futureYear = parseInt(currentYear) + parseInt(years);
     
+    let interest = (amount * rate)/100;
+    interest*=years;
+
+    let quote = `If you deposit ${amount}, <br>
+    at an in interest rate of ${rate}%. <br>
+    You will receive an amount of ${interest}, <br>
+    in the year ${futureYear}. <br>`;
+
+    document.getElementById("result").innerHTML =  quote;
+     
+    
+
 }
         
+
+
